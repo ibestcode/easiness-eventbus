c3118efe2512d57a9ab6d4f8456a1e996d3cea91
wfso <461663376@qq.com>
2019-11-18 21:33:54 +0800

    feat: 项目基本结构
    1. 确定了两个注解 @Subscribe 用于注解订阅了事件的函数，@EventListener用于注册事件监听器对象
    2. 确定两个调度器
    BreadthDispatcher 广度优先调度器 和 DepthDispatcher 深度优先调度器
    3. 创建
    EventBusException 事件总线异常
    4. 创建了一个全能的事件监听器 OmnipotentListener
    和一个事件监听器的注册器 ListenerRegistrar
    5. 创建了两个事件总线（EventBus)， AsyncEventBus
    异步事件总线，TransactionEventBus 事务事件总线

---
+++ src/main/java/cn/ibestcode/easiness/eventbus/listener/OmnipotentListener.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright 2019 the original author or authors.
+ * <p>
+ * Licensed to the IBESTCODE under one or more agreements.
+ * The IBESTCODE licenses this file to you under the MIT license.
+ * See the LICENSE file in the project root for more information.
+ */
+
+package cn.ibestcode.easiness.eventbus.listener;
+
+import cn.ibestcode.easiness.eventbus.exception.EventBusException;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
+/**
+ * @author WFSO (仵士杰)
+ * create by WFSO (仵士杰) at 2019/11/18 19:55
+ */
+public class OmnipotentListener implements Listener {
+
+  private final Class<?> supportType;
+  private final Object target;
+  private final Method method;
+
+  public OmnipotentListener(Object target, Method method) {
+    this.target = target;
+    this.method = method;
+
+    this.supportType = getMethodArgumentType(method);
+
+    assertPublicMethod(method);
+  }
+
+  protected Class getMethodArgumentType(Method method) {
+    Class[] paramTypes = method.getParameterTypes();
+    if (paramTypes.length != 1) {
+      throw new IllegalArgumentException("Method " + method + " has @Subscribe annotation but has " + paramTypes.length + " parameters."
+        + "Subscriber methods must have exactly 1 parameter.");
+    }
+    return paramTypes[0];
+  }
+
+  private void assertPublicMethod(Method method) {
+    int modifiers = method.getModifiers();
+    if (!Modifier.isPublic(modifiers)) {
+      throw new IllegalArgumentException("Event handler method [" + method + "] must be public.");
+    }
+  }
+
+  @Override
+  public Class<?> supports() {
+    return supportType;
+  }
+
+  @Override
+  public final void handle(Object event) {
+    try {
+      method.invoke(target, event);
+    } catch (IllegalAccessException e) {
+      throw new EventBusException(e);
+    } catch (InvocationTargetException e) {
+      throw new EventBusException(e.getCause());
+    }
+  }
+}
